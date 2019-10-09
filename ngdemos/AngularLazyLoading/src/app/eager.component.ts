b6b02cfb8efd9aad1c84499f370378e38d86bfe2
Wael Kdouh <waekdo@microsoft.com>
2019-10-09 10:45:57 +0100

    Removed the demos

--- ngdemos/AngularLazyLoading/src/app/eager.component.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  template: '<p>Eager Component</p>'
-})
-export class EagerComponent {}
--- ngdemos/AngularLazyLoading/src/app/lazy/lazy.component.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  template: '<p>Lazy Component</p>'
-})
-export class LazyComponent {}
